55955ec76e559c2a18d2286f8b4180c435fb6807
nandini224 <20951A6224@iare.ac.in>
2025-03-17 16:43:58 +0530

    Project Setup

---
+++ app/src/main/java/uk/ac/tees/mad/journeysnap/AppNavigation.kt
@@ -0,0 +1,9 @@
+package uk.ac.tees.mad.journeysnap
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppNavigation() {
+
+}
---
+++ app/src/main/java/uk/ac/tees/mad/journeysnap/utils/Constants.kt
@@ -0,0 +1,4 @@
+package uk.ac.tees.mad.journeysnap.utils
+
+object Constants {
+}
